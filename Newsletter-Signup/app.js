a2c81af17f4671710ef05022a32a0dc90b936f19
Dev Joshi <devjtitan@gmail.com>
2023-02-08 22:32:34 +0530

    Feat: Add name and email fields in the form

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -6,13 +6,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const request = require("request");
 
+app.use(express.static("Public"));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 });
 
-
-
-
+app.post("/", (req, res) => {
+    console.log(req.body.FirstName);
+    console.log(req.body.LastName);
+    console.log(req.body.Email);
+    res.send();
+})
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
