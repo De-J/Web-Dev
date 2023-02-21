2e090a58db0a470b2ec8b50e5a57b65cf463c018
Dev Joshi <devjtitan@gmail.com>
2023-02-21 18:29:03 +0530

    Setting up MongoDB backend and API to view/edit database

---
+++ RESTful_API/app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+
+
+//use 127.0.0.1 instead of localhost in the string below to avoid ECONNREFUSED error
+mongoose.connect("mongodb://127.0.0.1:27017/notesDB", {useNewUrlParser: true});
+
+const noteSchema = {
+    title: String,
+    content: String
+};
+
+const Note = mongoose.model("Article", noteSchema);
+
+app.get("/notes", (req, res) => {
+    Note.find((err, foundNotes) => {
+        res.send((!err) ? foundNotes : err);
+    });
+});
+
+app.post("/notes", (req, res) => {
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newNote = new Note({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newNote.save((err) => res.send(
+        (!err) ? "New note added successfully" : err
+    ));
+    //newNote.save().then(() => res.send("New note has been added successfully"), (err) => res.send(err));
+});
+
+
+app.delete("/notes", (req, res) => {
+    Note.deleteMany((err) => res.send(
+        (!err) ? "All notes deleted successfully" : err
+    ));
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
