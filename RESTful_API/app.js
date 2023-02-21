62934d1feae44e42c2bcb1f187457b632fed9711
Dev Joshi <devjtitan@gmail.com>
2023-02-21 18:44:57 +0530

    Chaining route handlers using app.route()

--- RESTful_API/app.js
+++ RESTful_API/app.js
@@ -24,32 +24,30 @@ const noteSchema = {
 
 const Note = mongoose.model("Article", noteSchema);
 
-app.get("/notes", (req, res) => {
-    Note.find((err, foundNotes) => {
-        res.send((!err) ? foundNotes : err);
+app.route("/notes")
+    .get((req, res) => {
+        Note.find((err, foundNotes) => {
+            res.send((!err) ? foundNotes : err);
+        });
+    })
+    .post((req, res) => {
+        console.log(req.body.title);
+        console.log(req.body.content);
+
+        const newNote = new Note({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newNote.save((err) => res.send(
+            (!err) ? "New note added successfully" : err
+        ));
+        //newNote.save().then(() => res.send("New note has been added successfully"), (err) => res.send(err));
+    })
+    .delete("/notes", (req, res) => {
+        Note.deleteMany((err) => res.send(
+            (!err) ? "All notes deleted successfully" : err
+        ));
     });
-});
-
-app.post("/notes", (req, res) => {
-    console.log(req.body.title);
-    console.log(req.body.content);
-
-    const newNote = new Note({
-        title: req.body.title,
-        content: req.body.content
-    });
-    newNote.save((err) => res.send(
-        (!err) ? "New note added successfully" : err
-    ));
-    //newNote.save().then(() => res.send("New note has been added successfully"), (err) => res.send(err));
-});
-
-
-app.delete("/notes", (req, res) => {
-    Note.deleteMany((err) => res.send(
-        (!err) ? "All notes deleted successfully" : err
-    ));
-});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
